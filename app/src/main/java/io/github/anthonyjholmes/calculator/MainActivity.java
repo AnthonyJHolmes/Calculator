33a7048a4ebadc074fbf003516a7ab7cb7e953fb
Anthony Holmes <a.j.holmes@umail.ucc.ie>
2016-01-28 23:55:55 +0000

    Validation for empty textfields, used toast to display messages Added clear button.

--- app/src/main/java/io/github/anthonyjholmes/calculator/MainActivity.java
+++ app/src/main/java/io/github/anthonyjholmes/calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonSubtraction;
     private Button buttonMultiplication;
     private Button buttonDivision;
+    private Button buttonClear;
     private TextView textResult;
 
     @Override
@@ -22,48 +24,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        operand1= (EditText) findViewById(R.id.firstNumber);
-        operand2= (EditText) findViewById(R.id.secondNumber);
+        operand1 = (EditText) findViewById(R.id.firstNumber);
+        operand2 = (EditText) findViewById(R.id.secondNumber);
         buttonAddition = (Button) findViewById(R.id.buttonAddition);
         buttonSubtraction = (Button) findViewById(R.id.buttonSubtraction);
         buttonMultiplication = (Button) findViewById(R.id.buttonMultiplication);
-        buttonDivision =(Button) findViewById(R.id.buttonDivision);
+        buttonDivision = (Button) findViewById(R.id.buttonDivision);
+        buttonClear = (Button) findViewById(R.id.buttonClear);
         textResult = (TextView) findViewById(R.id.textResult);
 
         buttonAddition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double oper1 = Double.parseDouble(operand1.getText().toString());
-                double oper2 = Double.parseDouble(operand2.getText().toString());
-                double theResult = oper1 + oper2;
-                textResult.setText(Double.toString(theResult));
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    double theResult = oper1 + oper2;
+                    textResult.setText(Double.toString(theResult));
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter two numbers", Toast.LENGTH_LONG).show();
+                }
             }
         });
         buttonSubtraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double oper1 = Double.parseDouble(operand1.getText().toString());
-                double oper2 = Double.parseDouble(operand2.getText().toString());
-                double theResult = oper1 - oper2;
-                textResult.setText(Double.toString(theResult));
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    double theResult = oper1 - oper2;
+                    textResult.setText(Double.toString(theResult));
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter two numbers", Toast.LENGTH_LONG).show();
+                }
             }
         });
         buttonMultiplication.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double oper1 = Double.parseDouble(operand1.getText().toString());
-                double oper2 = Double.parseDouble(operand2.getText().toString());
-                double theResult = oper1 * oper2;
-                textResult.setText(Double.toString(theResult));
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    double theResult = oper1 * oper2;
+                    textResult.setText(Double.toString(theResult));
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter two numbers", Toast.LENGTH_LONG).show();
+                }
             }
         });
         buttonDivision.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double oper1 = Double.parseDouble(operand1.getText().toString());
-                double oper2 = Double.parseDouble(operand2.getText().toString());
-                double theResult = oper1 / oper2;
-                textResult.setText(Double.toString(theResult));
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    double theResult = oper1 / oper2;
+                    textResult.setText(Double.toString(theResult));
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter two numbers", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.setText("");
+                operand2.setText("");
+                textResult.setText("0.00");
+                operand1.requestFocus();
             }
         });
     }
