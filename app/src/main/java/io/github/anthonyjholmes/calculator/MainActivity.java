df248d964f06a27bfba588beee8bc62d61902df5
Anthony Holmes <a.j.holmes@umail.ucc.ie>
2016-01-29 14:30:36 +0000

    added squared and cubed functionality. Changed the UI a bit

--- app/src/main/java/io/github/anthonyjholmes/calculator/MainActivity.java
+++ app/src/main/java/io/github/anthonyjholmes/calculator/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonMultiplication;
     private Button buttonDivision;
     private Button buttonClear;
+    private Button buttonSquared;
+    private Button buttonCubed;
     private TextView textResult;
 
     @Override
@@ -31,12 +33,14 @@ public class MainActivity extends AppCompatActivity {
         buttonMultiplication = (Button) findViewById(R.id.buttonMultiplication);
         buttonDivision = (Button) findViewById(R.id.buttonDivision);
         buttonClear = (Button) findViewById(R.id.buttonClear);
+        buttonSquared = (Button) findViewById(R.id.squared);
+        buttonCubed = (Button) findViewById(R.id.buttonCubed);
         textResult = (TextView) findViewById(R.id.textResult);
 
         buttonAddition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0) ) {
                     double oper1 = Double.parseDouble(operand1.getText().toString());
                     double oper2 = Double.parseDouble(operand2.getText().toString());
                     double theResult = oper1 + oper2;
@@ -86,12 +90,44 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        buttonSquared.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (operand1.getText().length() > 0){
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double theResult = oper1* oper1;
+                    textResult.setText(Double.toString(theResult));
+                }else if (operand2.getText().length() > 0){
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    double theResult = oper2* oper2;
+                    textResult.setText(Double.toString(theResult));
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter a number in either textfield", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        buttonCubed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (operand1.getText().length() > 0){
+                    double oper1 = Double.parseDouble(operand1.getText().toString());
+                    double theResult = oper1* oper1 *oper1;
+                    textResult.setText(Double.toString(theResult));
+                }else if (operand2.getText().length() > 0){
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+                    double theResult = oper2* oper2 *oper2;
+                    textResult.setText(Double.toString(theResult));
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter a number in either textfield", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
         buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 operand1.setText("");
                 operand2.setText("");
-                textResult.setText("0.00");
+                textResult.setText("0.0");
                 operand1.requestFocus();
             }
         });
